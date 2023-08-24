6416459786ff8b7fd801b04d527bcfc152019681
MaddyKm <134648462+MaddyKm@users.noreply.github.com>
2023-08-24 09:54:39 -0400

    adds into functions asking about the preferences to lump together to options

--- script.js
+++ script.js
@@ -17,56 +17,229 @@ var generateBtn = document.querySelector("#generate");
 //DEPENDENCIES DOM
 
 //DATA
-var numbers = "0 1 2 3 4 5 6 7 8 9".split("");
-var lowerletters = "q w e r t y u i o p l k j h g f d s a z x c v b n m".split(
-  ""
-);
-var upperLower =
-  "q w e r t y u i o p a s d f g h j k l z x c v b n m Q W E R T Y U I O P A S D F G H J K L Z X C V B N M".split(
-    ""
-  );
-var specialCharacters = "! @ # $ % ^ & * ( ) ~ - ? > < / . , ; ' { }".split("");
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerletters = [
+  "q",
+  "w",
+  "e",
+  "r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "l",
+  "k",
+  "j",
+  "h",
+  "g",
+  "f",
+  "d",
+  "s",
+  "a",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+  "n",
+  "m",
+];
+var upperLower = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+];
+var specialCharacters = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "~",
+  "-",
+  "?",
+  ">",
+  "<",
+  "/",
+  ".",
+  ",",
+  ";",
+  "'",
+  "{",
+  "}",
+];
+var characterOptions = [];
 //FUNCTIONS
 //How many Characters do you want for your password? (8-28)
 function charLength() {
   var amtChar = prompt("How many characters do you want in your password?");
+  var length = parseInt(amtChar);
   //If not between 8 and 28 ask for that
   if (amtChar < 8 || amtChar > 28) {
     alert("Password length must be between 8 and 28 characters.");
     writePassword();
   } else {
-    return amtChar;
+    return length;
   }
 }
 
-//Ask if you would like to include uppercase letters?
-function upperCase() {
-  if (confirm("Would you like to use uppercase letters?")) {
+//Ask if you want to use lowercase letters
+function lowerCase() {
+  if (confirm("Would you like to use lowercase letters?")) {
+    //if yes to lowercase add lowercase letters to character options
+    characterOptions.push(...lowerletters);
+    console.log(characterOptions);
     return true;
   } else {
     return false;
   }
 }
-
+//Ask if you would like to include uppercase letters?
+function upperCase() {
+  if (confirm("Would you like to use uppercase letters?")) {
+    //if yes to uppercase add uppercase letters to character options
+    characterOptions.push(...upperLower);
+    console.log(characterOptions);
+    return true;
+  } else {
+    return false;
+  }
+}
+function incNumb() {
+  if (confirm("Would you like to use numbers?")) {
+    //if yes to numbers add numbers to character options
+    characterOptions.push(...numbers);
+    console.log(characterOptions);
+    return true;
+  } else {
+    return false;
+  }
+}
 //Ask if you would like to include special characters?
 function specialChar() {
   if (confirm("Would you like to use special characters?")) {
+    //if yes to special characters add special characters to character options
+    characterOptions.push(...specialCharacters);
+    console.log(characterOptions);
     return true;
   } else {
     return false;
   }
 }
 //Generate the password using the preferences given
-function genPassword() {
-  CompOptions[Math.floor(Math.random() * CompOptions.length)];
-}
+
+// function genPassword(amtChar, length, includeUp, includeNum, includeSpcl) {
+//   if (includeUp === true && includeNum === true && includeSpcl === true) {
+//     for (let i = 0; i < amtChar; i++) {
+//       var password = (upperLower + numbers + specialCharacters)[
+//         Math.floor(Math.random() * specialCharacters.length)
+//       ];
+//     }
+//     return password;
+//   } else if (
+//     includeUp === true &&
+//     includeNum === true &&
+//     includeSpcl === false
+//   ) {
+//     var password = (upperLower + numbers)[Math.floor(Math.random() * length)];
+//     return password;
+//   } else if (
+//     includeUp === true &&
+//     includeNum === false &&
+//     includeSpcl === false
+//   ) {
+//     password = upperLower[Math.floor(Math.random() * length)];
+//     return password;
+//   } else if (
+//     includeUp === false &&
+//     includeNum === false &&
+//     includeSpcl === false
+//   ) {
+//     for (let i = 0; i < amtChar; i++) {
+//       password = lowerletters[i][Math.floor(Math.random() * length)];
+//     }
+//     return password;
+//   } else if (
+//     includeUp === false &&
+//     includeNum === true &&
+//     includeSpcl === true
+//   ) {
+//     password = (lowerletters + numbers + specialCharacters)[
+//       Math.floor(Math.random() * length)
+//     ];
+//     return password;
+//   } else if (
+//     includeUp === false &&
+//     includeNum === true &&
+//     includeSpcl === false
+//   ) {
+//     password = (lowerletters + numbers)[Math.floor(Math.random() * length)];
+//     return password;
+//   } else if (
+//     includeUp === false &&
+//     includeNum === false &&
+//     includeSpcl === true
+//   ) {
+//     password = (lowerletters + specialCharacters)[
+//       Math.floor(Math.random() * length)
+//     ];
+//     return password;
+//   } else {
+//     password = (upperLower + specialCharacters)[
+//       Math.floor(Math.random() * length)
+//     ];
+//     return password;
+//   }
+//}
 function writePassword() {
   var amtChar = charLength();
   console.log(amtChar);
+  var includeLwr = lowerCase();
+  console.log(includeLwr);
   var includeUp = upperCase();
   console.log(includeUp);
+  var includeNum = incNumb();
+  console.log(includeNum);
   var includeSpcl = specialChar();
   console.log(includeSpcl);
+  var password = genPassword(
+    amtChar,
+    includeUp,
+    includeNum,
+    includeSpcl,
+    length
+  );
+  console.log(password);
 }
 //USER INTERACTIONS
 
